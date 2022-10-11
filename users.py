bd72870510b5398d3786c2326e33378156e49216
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-10-11 13:10:57 +0600

    Update users.py

--- users.py
+++ users.py
@@ -61,5 +61,21 @@ def second(message):
 def third(message):
 
     bot.send_message(chat_id=473641296, text=message.text)
+    
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
 
-bot.polling()
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
