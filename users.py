fdb813393ca13554146fca7384e049cc8a1435ac
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-10-11 17:27:08 +0600

    Update users.py

--- users.py
+++ users.py
@@ -12,10 +12,11 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 chat_id = '473641296'
 
-knopka1 = 'Ректор'
-knopka2 = 'Проректор1'
-knopka3 = 'Проректор2'
-knopka4 = 'Проректор3'
+knopka1 = 'Академиялық қызмет жөніндегі проректор'
+knopka2 = 'Инфрақұрылымдық даму жөніндегі проректор'
+knopka3 = 'Ғылыми жұмыс жөніндегі проректор'
+knopka4 = 'Стратегиялық даму жөніндегі проректор'
+knopka5 = 'Басқа құрылымға'
 
 
 @bot.message_handler(commands=["start"])
@@ -25,7 +26,7 @@ def first(message):
     keyboard.add(knopka2)
     keyboard.add(knopka3)
     keyboard.add(knopka4)
-    send = bot.send_message(message.chat.id, 'Кім керек соны астынан батырманы таңдаңыз', parse_mode='Markdown',
+    send = bot.send_message(message.chat.id, 'Қажет құрылымды таңдаңыз!', parse_mode='Markdown',
                             reply_markup=keyboard)
     bot.register_next_step_handler(send, second)
 
@@ -64,6 +65,15 @@ def second(message):
         keyboard = types.ReplyKeyboardMarkup(True, False)
         keyboard.add("/start")
         bot.send_message(chat_id=473641296, text=knopka4)
+        send = bot.send_message(message.chat.id,
+                                'Аты-жөніңізді, почтаңызды және ұсынысыңызды жазыңыз болмаса кездесу күнін белгілеңіз (День.Месяц.Год',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, third)
+    
+   elif message.text == knopka5:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        keyboard.add("/start")
+        bot.send_message(chat_id=473641296, text=knopka5)
         send = bot.send_message(message.chat.id,
                                 'Аты-жөніңізді, почтаңызды және ұсынысыңызды жазыңыз болмаса кездесу күнін белгілеңіз (День.Месяц.Год',
                                 parse_mode='Markdown', reply_markup=keyboard)
