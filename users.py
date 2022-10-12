fc541893a5bfa7543dc95b023e45c695aef667ed
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-10-12 17:19:07 +0600

    Update users.py

--- users.py
+++ users.py
@@ -41,7 +41,9 @@ def eki(message):
     if message.text == knopka6:
         keyboard = types.ReplyKeyboardMarkup(True, False)
         #bot.send_message(chat_id=473641296, text=knopka1)
-        send = bot.send_message( parse_mode='Markdown', reply_markup=keyboard)
+        send = bot.send_message(message.chat.id,
+                                'kaz язык',
+                                parse_mode='Markdown', reply_markup=keyboard)
         bot.register_next_step_handler(send, first)
 
     elif message.text == knopka7:
