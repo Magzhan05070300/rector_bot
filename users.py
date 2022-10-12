8becea89dd10513cfb9d8e6250cc3cf08595ff55
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-10-12 16:38:11 +0600

    Update users.py

--- users.py
+++ users.py
@@ -12,14 +12,52 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 chat_id = '473641296'
 
-knopka1 = 'Академиялық қызмет жөніндегі проректор'
+knopka1 = '3a3Академиялық қызмет жөніндегі проректор'
 knopka2 = 'Инфрақұрылымдық даму жөніндегі проректор'
 knopka3 = 'Ғылыми жұмыс жөніндегі проректор'
 knopka4 = 'Стратегиялық даму жөніндегі проректор'
 knopka5 = 'Басқа құрылымға'
-
+knopka6 = 'Қазақ тілі'
+knopka7 = 'Русский язык'
+knopka8 = 'Проректор по академическим вопросам'
+knopka9 = 'Проректор по инфраструктурному развитию'
+knopka10 = 'Проректор по науке'
+knopka11 = 'Проректор по стратегическому развитию'
+knopka12 = 'Другие подразделения'
 
 @bot.message_handler(commands=["start"])
+def bir(message):
+    keyboard = types.ReplyKeyboardMarkup(True, False)
+    keyboard.add(knopka6)
+    keyboard.add(knopka7)
+
+    send = bot.send_message(message.chat.id, 'Қажет тілді таңдаңыз! Выберите нужный язык!', parse_mode='Markdown',
+                            reply_markup=keyboard)
+    bot.register_next_step_handler(send, eki)
+
+def eki(message):
+    # bot.send_message(chat_id=chat_id, text=message.text)
+
+    if message.text == knopka6:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        #bot.send_message(chat_id=473641296, text=knopka1)
+        send = bot.send_message(message.chat.id,
+                                'Қазақ тілі',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, first)
+
+    elif message.text == knopka7:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        #bot.send_message(chat_id=473641296, text=knopka1)
+        send = bot.send_message(message.chat.id,
+                                'Русский язык',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, odin)
+
+    else:
+        send = bot.send_message(message.chat.id, 'Нажмите на нужную кнопку')
+        bot.register_next_step_handler(send, eki)
+
 def first(message):
     keyboard = types.ReplyKeyboardMarkup(True, False)
     keyboard.add(knopka1)
@@ -27,7 +65,7 @@ def first(message):
     keyboard.add(knopka3)
     keyboard.add(knopka4)
     keyboard.add(knopka5)
-    
+
     send = bot.send_message(message.chat.id, 'Қажет құрылымды таңдаңыз!', parse_mode='Markdown',
                             reply_markup=keyboard)
     bot.register_next_step_handler(send, second)
@@ -86,9 +124,75 @@ def second(message):
         bot.register_next_step_handler(send, second)
 
 
+
+def odin(message):
+    keyboard = types.ReplyKeyboardMarkup(True, False)
+    keyboard.add(knopka8)
+    keyboard.add(knopka9)
+    keyboard.add(knopka10)
+    keyboard.add(knopka11)
+    keyboard.add(knopka12)
+
+    send = bot.send_message(message.chat.id, 'Выберите нужное Вам подразделение!', parse_mode='Markdown',
+                            reply_markup=keyboard)
+    bot.register_next_step_handler(send, second)
+
+
+def dva(message):
+    # bot.send_message(chat_id=chat_id, text=message.text)
+
+    if message.text == knopka8:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        keyboard.add("/start")
+        bot.send_message(chat_id=473641296, text=knopka8)
+        send = bot.send_message(message.chat.id,
+                                'Просим оставить информацию:'+str('1. ФИО'/n) + str('2. Заявитель (Студент, сотрудник)'/n)+str('3. Дата желаемого визита'),
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, third)
+
+    elif message.text == knopka9:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        keyboard.add("/start")
+        bot.send_message(chat_id=473641296, text=knopka9)
+        send = bot.send_message(message.chat.id,
+                                'Просим оставить информацию: (День.Месяц.Год)',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, third)
+
+    elif message.text == knopka10:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        keyboard.add("/start")
+        bot.send_message(chat_id=473641296, text=knopka10)
+        send = bot.send_message(message.chat.id,
+                                'Просим оставить информацию: (День.Месяц.Год)',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, third)
+
+    elif message.text == knopka11:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        keyboard.add("/start")
+        bot.send_message(chat_id=473641296, text=knopka11)
+        send = bot.send_message(message.chat.id,
+                                'Просим оставить информацию: (День.Месяц.Год)',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, third)
+
+    elif message.text == knopka12:
+        keyboard = types.ReplyKeyboardMarkup(True, False)
+        keyboard.add("/start")
+        bot.send_message(chat_id=473641296, text=knopka12)
+        send = bot.send_message(message.chat.id,
+                                'Просим оставить информацию: (День.Месяц.Год)',
+                                parse_mode='Markdown', reply_markup=keyboard)
+        bot.register_next_step_handler(send, third)
+
+    else:
+        send = bot.send_message(message.chat.id, 'Нажмите кнопку пожалуйста!')
+        bot.register_next_step_handler(send, dva)
+
+
 def third(message):
     bot.send_message(chat_id=473641296, text=message.text)
-
 
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
